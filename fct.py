bd3cb90d53c76093a7244f20c5c6622af261277f
LilouSoulas <lilou.soulas@gmail.com>
2020-12-09 14:54:08 +0100

    Fonction creerSalle2

--- fct.py
+++ fct.py
@@ -32,6 +32,40 @@ def creerSalle(densite,Ly,Lx):
     return salle
 
 
+#Creation d'une salle remplie d'automates dont on choisi le nombre
+def Creeralle2(nb, Ly, Lx): #Attention, nb doit etre inferieur à (Lx)*(Ly)
+    ligneMur = [3]*(Ly+2)
+    colonneMur = [3]*Lx
+    #Rempli la salle avec un nombre nb de gens
+    salle=np.zeros((Ly, Lx))
+    x_reinjection_min=(len(salle)-1)
+    x_reinjection_max=0
+    y_reinjection_min=0
+    y_reinjection_max=len(TM-1)
+
+    new_x=new_y=0
+    for i in range(nb):
+        new_x=randint(x_reinjection_min, x_reinjection_max)
+        new_y=randint(y_reinjection_min, y_reinjection_max)
+        
+        while(salle[new_x,new_y]!=0):
+            new_x=randint(x_reinjection_min, x_reinjection_max)
+            new_y=randint(y_reinjection_min, y_reinjection_max) 
+
+        salle[new_x,new_y]=1
+            
+
+    #créer des murs en gris
+    salle = np.column_stack((colonneMur, salle, colonneMur))
+    salle = np.vstack((ligneMur, salle, ligneMur))
+
+    #créer une porte representée en rouge
+    salle[0,int(Lx/2)]=2
+    salle[0,int((Lx-1)/2)]=2
+    return salle
+
+
+
 #Afficher SFF
 def afficheSFF(salle):
     Ly=len(salle)
@@ -292,6 +326,7 @@ def resolution2(TM,k,u):
     plt.clf()
     return Nb
         
+
 @timer
 def resolv2(TM,k,u):
     Nb=0
@@ -314,14 +349,18 @@ def SimulationsU(d,taille,k,Npas,Nsim):
     u=np.linspace(0,1,Npas)
     Ntours=np.asarray([[resolv2(Terrain,k,ut) for Terrain in Terrains] for ut in u])
     N=np.zeros(Npas)
+    ecart=np.zeros(Npas)
+
     for i in range(Npas):
         N[i]=Ntours[i].sum()/Ntours[i].size
+        ecart[i]=ecartType(Ntours[i])
 
     fig = plt.figure(figsize=(15,10))
     plt.plot(u,N)
     plt.xlabel("U")
     plt.ylabel("Nombre de tours")
     plt.title(f"Nombre de tour mis pour purger une piece de taille {taille} et densité {d}, en fonction de u (coeff de friction), pour k={k} ({Nsim} simulations par pas, {Npas} pas de u)")
+    plt.errorbar(u, N, yerr=ecart, fmt = 'none', capsize = 10, ecolor = 'red', zorder = 1)
     plt.show()
 
 
@@ -352,10 +391,7 @@ def SimulationsK(d,taille,u,kmin,kmax,Npas,Nsim):
 
     for i in range(Npas):
         N[i]=Ntours[i].sum()/Ntours[i].size
-        print(Ntours[i])
         ecart[i]=ecartType(Ntours[i])
-
-    
 
     fig = plt.figure(figsize=(15,10))
     plt.plot(k,N)
@@ -365,31 +401,24 @@ def SimulationsK(d,taille,u,kmin,kmax,Npas,Nsim):
     plt.errorbar(k, N, yerr=ecart, fmt = 'none', capsize = 10, ecolor = 'red', zorder = 1)
     plt.show()
 
-
-
-
-
-
-
-
-
-
-
-
 @timer    
 def SimulationsUR(d,taille,k,Npas,Nsim):
     Terrains=np.asarray([creerSalle(d,taille[0],taille[1]) for i in range(Nsim)])
     u=np.linspace(0,1,Npas)
     Ntours=np.asarray([[resolv2(Terrain,k,ut) for Terrain in Terrains] for ut in u])
     N=np.zeros(Npas)
+    ecart=np.zeros(Npas)
     for i in range(Npas):
         N[i]=Ntours[i].sum()/Ntours[i].size
+        ecart[i]=ecartType(Ntours[i])
+
 
     fig = plt.figure(figsize=(15,10))
     plt.plot(u,N*0.27)
     plt.xlabel("U")
     plt.ylabel("Temps (en s)")
     plt.title(f"Nombre de tour mis pour purger une piece de taille {taille[0]*0.4,taille[1]*0.4} mètres et densité {d}, en fonction de u (coeff de friction), pour k={k} ({Nsim} simulations par pas, {Npas} pas de u)")
+    plt.errorbar(u, N*0.27, yerr=ecart, fmt = 'none', capsize = 10, ecolor = 'red', zorder = 1)
     plt.show()
 
 
@@ -399,12 +428,15 @@ def SimulationsKR(d,taille,u,kmin,kmax,Npas,Nsim):
     k=np.linspace(kmin,kmax,Npas)
     Ntours=np.asarray([[resolv2(Terrain,kt,u) for Terrain in Terrains] for kt in k])
     N=np.zeros(Npas)
+    ecart=np.zeros(Npas)
     for i in range(Npas):
         N[i]=Ntours[i].sum()/Ntours[i].size
+        ecart[i]=ecartType(Ntours[i])
 
     fig = plt.figure(figsize=(15,10))
     plt.plot(k,N*0.27)
     plt.xlabel("k")
     plt.ylabel("Temps (en s)")
     plt.title(f"Nombre de tour mis pour purger une piece de taille {taille[0]*0.4,taille[1]*0.4} mètres et densité {d}, en fonction de k, pour u={u} ({Nsim} simulations par pas, {Npas} pas de k, et k variant de {kmin} a {kmax})")
+    plt.errorbar(k, N*0.27, yerr=ecart, fmt = 'none', capsize = 10, ecolor = 'red', zorder = 1)
     plt.show()
