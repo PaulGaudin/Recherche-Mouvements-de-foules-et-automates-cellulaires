836955fc5ce462a3a3cbccf630683e7b42c95ae1
LilouSoulas <lilou.soulas@gmail.com>
2021-01-04 15:00:42 +0100

    Update NouvSim.py
    voili voilou

--- NouvSim.py
+++ NouvSim.py
@@ -35,3 +35,71 @@ def creerSalle(nb, Ly, Lx): #Attention, nb doit etre inferieur à (Lx)*(Ly)
     salle[0,int(Lx/2)]=2
     salle[0,int((Lx-1)/2)]=2
     return salle
+
+
+    #Affiche chaque tour jusqu'a ce que tout les automates soient sortis
+def resolutionSansVideo(TM,k,u):
+    evol=[TM]
+    Nb=0
+    while((TM==Init(TM.shape[0]-2,TM.shape[1]-2)).all()!=1):
+        TM=Deplacement(TM,k,u)
+        evol.append(TM)
+        Nb+=1
+    return Nb
+
+#fct qui fait tourner un certain nbr de fois resolution 1 pour une piece creée avec creerSalle mais qui ne l'affiche pas
+def faisTourner(nbP, taille, k, u, nbSim ):
+    temps=[]  
+    for i in range(nbSim):
+        TM=creerSalle2(nbP, taille[0], taille[1])
+        #resolution(TM, k, u) renvoi le nombre de tours que prend la resolution pour que tout le monde sorte
+        temps.append((resolutionSansVideo(TM, k, u))*0.27)
+        #ce qui suit sert a visualiser l'avancée du programme dans la compilation
+        if((i%10)==0):
+            print("itérations n°",i)
+
+    max=np.max(temps)
+    min=np.min(temps)
+    #On creer un nombre qui est l'intier directeent superieur à max_temps
+    sup=int(max)+1
+    inf=int(min)
+    nbValeurs, moy, mediane, quartils, var, ecart = etudeStat(temps)
+
+    plt.figure(1)
+    plt.hist(temps, range = (inf, sup), bins = ((sup-inf)), color = 'blue')
+    plt.title(f"Temps du dernier sorti d'une piece {taille[0]*0.4, taille[1]*0.4}en m, pour {nbP} personnes initiales, pour k={k} et u={u}, pour {nbSim} simulations")
+    plt.xlabel("temps en secondes")
+    plt.ylabel("nombre de simulation atteigant le même temps de sorti")
+    plt.figure(2)
+    plt.figtext(0.05, 0.3, f'ETUDE STATISTIQUE \n  \n Nombre de valeurs: {nbValeurs} \n \n Moyenne={moy}s \n  \n Mediane={mediane}s \n  \n Variance={var} \n  \n Ecart Type={ecart}s  \n  \n Premier quartils={quartils[0]}s \n  \n Deuxième quartils={quartils[1]}s \n  \n Troisième quartils={quartils[2]}s' )
+    plt.axis("off")
+    plt.plot()
+    plt.show()
+
+    return temps
+
+
+def etudeStat(tableau):
+
+    #On défini la moyenne du tableau:
+    moy=0
+    for i in range(len(tableau)):
+        moy=moy+tableau[i]
+    moy=moy/len(tableau)
+    #On definit la variance
+    var=0
+    for i in range(len(tableau)):
+        var=var + (tableau[i]-moy)**2
+    var=var/len(tableau)
+    #Puis on retourn l'écart type qui est la racine carrée de la variance
+    ecart=np.sqrt(var)    
+    #quartils
+    quartils=[]
+    quartils.append(np.quantile(tableau, .25))
+    quartils.append(np.quantile(tableau, .50))
+    quartils.append(np.quantile(tableau, .75))
+    #mediane
+    mediane=np.median(tableau)
+    
+
+    return len(tableau), moy, mediane, quartils, var, ecart #retourne, le nb de valeurs, la moyenne, la variance, l'écart type
