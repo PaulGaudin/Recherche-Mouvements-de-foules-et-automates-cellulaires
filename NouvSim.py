f95ad91175b1de854f1a0dccd8bbd408cc0b177c
PaulGaudin <74604600+PaulGaudin@users.noreply.github.com>
2021-01-21 18:31:45 +0100

    Petites corrections

--- NouvSim.py
+++ NouvSim.py
@@ -11,9 +11,9 @@ def creerSalle(nb, Ly, Lx): #Attention, nb doit etre inferieur à (Lx)*(Ly)
     print(len(salle))
     
     x_reinjection_min=0
-    x_reinjection_max=(len(salle)-1)
+    x_reinjection_max=len(salle)-1
     y_reinjection_min=0
-    y_reinjection_max=len(salle-1)
+    y_reinjection_max=len(salle)-1
 
     new_x=new_y=0
     for i in range(nb):
@@ -22,10 +22,9 @@ def creerSalle(nb, Ly, Lx): #Attention, nb doit etre inferieur à (Lx)*(Ly)
         
         while(salle[new_x,new_y]!=0):
             new_x=randint(x_reinjection_min, x_reinjection_max)
-            new_y=randint(y_reinjection_min, y_reinjection_max) 
+            new_y=randint(y_reinjection_min, y_reinjection_max)
 
         salle[new_x,new_y]=1
-            
 
     #créer des murs en gris
     salle = np.column_stack((colonneMur, salle, colonneMur))
